acb6df612e7aa0e2b91126a02eebf642683bcb71
Minh Sang <109707462+minhsang1311@users.noreply.github.com>
2024-08-02 10:46:48 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url ='mongodb+srv://sangnnmsec:minh1sang1@minhsang.l9mkxog.mongodb.net/?retryWrites=true&w=majority&appName=MinhSang'
+const url ='mongodb+srv://sangnnmsec:minh1sang1@taxtuki.469o0bk.mongodb.net/?retryWrites=true&w=majority&appName=taxTuki'
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(url, {
